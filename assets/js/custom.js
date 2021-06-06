dde33edfee1dfeae4b4c306855587e88d6f8e72e
dukesteban <dukesteban@gmail.com>
2021-06-05 21:01:03 -0300

    primer commit

---
+++ assets/js/custom.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.service-wrapper', { delay: 400 });
+ScrollReveal().reveal('.service-wrapper-ofertas', { delay: 700 });
+ScrollReveal().reveal('.service-wrapper-ofertas-images', { delay: 1000 });
+ScrollReveal().reveal('.productos-wrapper', { delay: 400 });
+ScrollReveal().reveal('.productos-wrapper-images', { delay: 700 });
